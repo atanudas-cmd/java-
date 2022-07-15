19727c012e3bb9fe7eb871833551bc284bd92b75
atanudas-cmd <98704596+atanudas-cmd@users.noreply.github.com>
2022-07-15 11:07:01 +0530

    Add files via upload

---
+++ MyName.java
@@ -0,0 +1,16 @@
+//Write a function to print your name and call the function from main method//
+
+
+
+
+class MyName {
+
+    static void myMethod() {
+        System.out.println("My name is: Atanu");
+    }
+
+    public static void main(String[] args) {
+        myMethod();
+    }
+}
+
