1f7492c1101a5f95d3302429a6a849007b9e708d
atanudas-cmd <98704596+atanudas-cmd@users.noreply.github.com>
2022-07-15 09:48:19 +0530

    Add files via upload

---
+++ CommentEx2.java
@@ -0,0 +1,15 @@
+//Write a program for a Single line comment, multi-line and documentation comments//
+
+ class CommentEx2 
+	{    
+		public static void main(String[] args) 
+		{    
+				/* Let's declare and print variable in java. */
+
+    
+  				int i=10;    
+    				System.out.println(i);    
+								/* float j = 5.9;  float k = 4.4; 
+    				System.out.println( j + k ); */    
+		}    
+	}   
